fba4e2d25922c1f6c9481602d3e78a4cd119325a
csapo_nandor <nandor.csapo96@gmail.com>
2024-10-03 09:33:08 +0200

    clear

--- CodeReflectionExample/Program.cs
+++ CodeReflectionExample/Program.cs
@@ -6,8 +6,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var zoo = new ZooFactory();
-        zoo.MakeAnimalSpeak();
-        Console.ReadKey();
+
     }
 }
